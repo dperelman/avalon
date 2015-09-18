3dc5568b54253522299d30416b918e6be2a7fd9c
Alan Tran <alan.trann@gmail.com>
2015-09-17 19:46:57 -0700

    Added game and player mongo collections. generate functions for game and player

--- avalon.js
+++ avalon.js
@@ -1,3 +1,6 @@
+Games = new Mongo.Collection("games");
+Players = new Mongo.Collection("players");
+
 if (Meteor.isClient) {
   Template.body.created = function () {
     Session.set("currentView", "startMenu");
@@ -29,10 +32,61 @@ if (Meteor.isClient) {
       Session.set("currentView", "startMenu");
     }
   });
+
+  Template.newGame.events({
+    'submit .new-game-form': function (event) {
+      event.preventDefault();
+      var playerName = $(event.currentTarget).find('.name-input').val();
+
+      if (!playerName) {
+        return false;
+      }
+
+      var game = generateNewGame();
+      var player = generateNewPlayer(game, playerName);
+      debugger;
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function generateNewGame() {
+  var game = {
+    accessCode: generateAccessCode(),
+    numPlayers: 0,
+    state: "lobby"
+  };
+
+  var gameID = Games.insert(game);
+  game = Games.findOne(gameID);
+  return game;
+}
+
+function generateAccessCode() {
+  var code = "";
+  var alpha = "abcdefghjiklmnopqrstuvwxyz";
+
+  for (var i = 0; i < 6; i++) {
+    code += alpha.charAt(Math.floor(Math.random() * alpha.length));
+  }
+
+  return code;
+}
+
+function generateNewPlayer(game, name) {
+  var player = {
+    gameID: game._id,
+    name: name,
+    roll: null,
+    team: null
+  };
+
+  var playerID = Players.insert(player);
+  player = Players.findOne(playerID);
+  return player;
 }
